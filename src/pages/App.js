d89bbaefd90898c9f523e05ff69e25d8ac059abb
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2021-10-26 13:19:52 +0700

    ui header

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,13 +1,35 @@
-import './App.css'
-import { Switch, Route, Redirect } from 'react-router-dom'
+import './App.scss'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import Marketplace from './Marketplace'
 import Sell from './Sell'
 import Create from './Create'
+import { useHistory, useLocation } from 'react-router'
 
 function App() {
+  const { pathname } = useLocation()
+  const history = useHistory()
+  console.log(`pathname`, pathname)
+  const activeIndex =
+    pathname === '/marketplace' ? 0 : pathname === '/sell' ? 1 : pathname === '/create' ? 2 : undefined
   return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
+    <div className="app">
+      <header className="header">
+        <div />
+        <nav className="nav">
+          <ul>
+            <li className={activeIndex === 0 ? 'active' : ''} onClick={() => history.push('/marketplace')}>
+              Marketplace
+            </li>
+            <li className={activeIndex === 1 ? 'active' : ''} onClick={() => history.push('/sell')}>
+              Sell
+            </li>
+            <li className={activeIndex === 2 ? 'active' : ''} onClick={() => history.push('/create')}>
+              Create
+            </li>
+          </ul>
+        </nav>
+        <div class="account"></div>
+      </header>
       <Switch>
         <Route exact strict path="/marketplace" component={Marketplace} />
         <Route exact strict path="/sell" component={Sell} />
