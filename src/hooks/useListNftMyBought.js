1ab609b1a4763049cdb7d9c3d9ecf3f6e33c9a6f
thanhnv <thanhnv2@vnext.com.vn>
2021-11-04 09:50:10 +0700

    update code

--- src/hooks/useListNftMyBought.js
+++ src/hooks/useListNftMyBought.js
@@ -13,31 +13,52 @@ const useListNftMyBought = () => {
     ;(async () => {
       if (nftContract && nftMarketContract) {
         // get list tokens
-        const listItems = await nftMarketContract.fetchMyNFTs()
-        const data = await Promise.all(
-          listItems.map(async (i) => {
-            const tokenUri = await nftContract.tokenURI(i.tokenId)
-            const tokenState = await nftContract._tokenDetails(i.tokenId)
-            const meta = await axios.get(tokenUri)
-            let price = ethers.utils.formatUnits(i.minPrice.toString(), 'ether')
-            let item = {
-              id: i.itemId.toString(),
-              price,
-              tokenId: i.tokenId.toNumber(),
-              seller: i.seller,
-              buyer: i.buyer,
-              image: meta.data.urlImage,
-              class: meta.data.classId,
-              level: tokenState.level.toString(),
-              heath: tokenState.heath.toString(),
-              morale: tokenState.morale.toString(),
-              skill: tokenState.skill.toString(),
-              speed: tokenState.speed.toString(),
+        const listMyBoughtItems = await nftMarketContract.fetchMyNFTs()
+        const listMySellItems = await nftMarketContract.fetchItemsCreated()
+        const myItem = listMyBoughtItems.filter((buyItem) => {
+          console.log('buyItem', buyItem)
+          const buyTokenId = buyItem.tokenId.toNumber()
+          const buyId = buyItem.itemId.toNumber()
+          const sellItems = listMySellItems.filter((sellItem) => {
+            const sellTokenId = sellItem.tokenId.toNumber()
+            const sellId = sellItem.itemId.toNumber()
+            if(sellTokenId === buyTokenId && sellId > buyId){
+              return true
             }
-            return item
+            return false
           })
-        )
-        setList(data)
+          if(!sellItems.length){
+            return true
+          }
+          return false
+        })
+        console.log('listMyBoughtItems', listMyBoughtItems)
+        console.log('listMySellItems', listMySellItems)
+        console.log('myItem', myItem)
+        // const data = await Promise.all(
+        //   listItems.map(async (i) => {
+        //     const tokenUri = await nftContract.tokenURI(i.tokenId)
+        //     const tokenState = await nftContract._tokenDetails(i.tokenId)
+        //     const meta = await axios.get(tokenUri)
+        //     let price = ethers.utils.formatUnits(i.minPrice.toString(), 'ether')
+        //     let item = {
+        //       id: i.itemId.toString(),
+        //       price,
+        //       tokenId: i.tokenId.toNumber(),
+        //       seller: i.seller,
+        //       buyer: i.buyer,
+        //       image: meta.data.urlImage,
+        //       class: meta.data.classId,
+        //       level: tokenState.level.toString(),
+        //       heath: tokenState.heath.toString(),
+        //       morale: tokenState.morale.toString(),
+        //       skill: tokenState.skill.toString(),
+        //       speed: tokenState.speed.toString(),
+        //     }
+        //     return item
+        //   })
+        // )
+        // setList(data)
         return true
       }
     })()
