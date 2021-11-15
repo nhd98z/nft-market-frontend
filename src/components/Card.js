73a9debd548af6fc1fdbe0c485572a508e696c81
thangmd <thangmd@vnext.vn>
2021-11-15 09:15:36 +0700

    copy and link tx

--- src/components/Card.js
+++ src/components/Card.js
@@ -18,6 +18,7 @@ import { ReactComponent as Mech } from '../assets/mech.svg'
 import { ReactComponent as Bug } from '../assets/bug.svg'
 import useCancelMarketItem from '../hooks/useCancelMarketItem'
 import useLevelUp from '../hooks/useLevelUp'
+import {getTxPending} from '../utils/index'
 const StyledCard = styled(Box)`
   height: 320px;
   width: 225px;
@@ -87,180 +88,193 @@ export default forwardRef(function Card(props, ref) {
       <Mech />
     ) : null
 
+
+  function copyBuyer(buyer) {
+    
+    navigator.clipboard.writeText(buyer);
+  }
+
+  function inforTx() {
+    const tx= getTxPending()[0];
+    window.open("https://rinkeby.etherscan.io/tx/" + tx);
+    }
+
+
   return (
-    <StyledCard {...props}>
-      <Box padding="16px" width="100%">
-        <Box display="flex" justifyContent="space-between">
-          <Box>
-            <div># {item.tokenId}</div>
-            <Box display="flex" alignItems="flex-end" marginTop="4px">
-              {icon}
-              <Typography marginLeft="4px" fontSize="14px" lineHeight="normal">
-                {Object.keys(ClassItem).map((i) => {
-                  if (ClassItem[i] === item.class) {
-                    return t(i)
-                  }
-                  return ''
-                })}
-              </Typography>
-            </Box>
-          </Box>
-          {isMyNft && showBuyOrSellButton && (
-            <Box>
-              <MI.ControlPoint
-                cursor="pointer"
-                fontSize="large"
-                onClick={() => {
-                  onLevelUp(item.tokenId)
-                }}
-              />
-            </Box>
-          )}
-          <Box>
-            <Box display="flex">
-              <Box display="flex" alignItems="flex-start">
-                <MI.Favorite fontSize="small" style={{ fill: '#3ac279' }} />
-                <Typography fontSize="16px" lineHeight="normal">
-                  {item.heath}
-                </Typography>
-              </Box>
-              <Box display="flex" alignItems="flex-start">
-                <MI.FlashOn fontSize="small" style={{ fill: '#f7ac0a' }} />
-                <Typography fontSize="16px" lineHeight="normal">
-                  {item.speed}
-                </Typography>
-              </Box>
-            </Box>
-            <Box display="flex">
-              <Box display="flex" alignItems="flex-start">
-                <MI.StarRate fontSize="small" style={{ fill: '#9166e0' }} />
-                <Typography fontSize="16px" lineHeight="normal">
-                  {item.skill}
-                </Typography>
-              </Box>
-              <Box display="flex" alignItems="flex-start">
-                <MI.LocalFireDepartment fontSize="small" style={{ fill: '#c23a3a' }} />
-                <Typography fontSize="16px" lineHeight="normal">
-                  {item.morale}
-                </Typography>
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-        <Box width="100%" display="flex" flexDirection="column" justifyContent="center" alignItems="center" flex="1">
-          <StyledImage
-            style={{
-              backgroundImage: `url("${item.image}")`,
-            }}
-          ></StyledImage>
-          {/* <img src={item.image} alt="axie1" style={{ height: imageWidth ?? '160px', width: 'fit-content', margin: '8px' }} /> */}
-          {showBuyOrSellButton && isSell ? (
-            <CssTextField
-              width="50%"
-              unit="ETH"
-              type="number"
-              label={t('Price')}
-              variant="outlined"
-              myBackgroundColor="#000000"
-              myColor="#000000"
-              value={sellPrice}
-              onChange={(e) => {
-                setSellPrice(e.target.value)
-              }}
-            />
-          ) : (
-            <Typography fontSize="20px" fontWeight={400}>
-              {item.price && item.price + ' ETH'}
-            </Typography>
-          )}
-        </Box>
-        {!account && showBuyOrSellButton && (
-          <StyledButton variant="contained" style={{ margin: '8px 0' }} onClick={connectWallet}>
-            {t('Connect Metamask')}
-          </StyledButton>
-        )}
-        {account && !isApprove && showBuyOrSellButton && isSell && (
-          <StyledButton variant="contained" style={{ margin: '8px 0' }} onClick={onApprove}>
-            {t('Approve NFT')}
-          </StyledButton>
-        )}
-        {account && showBuyOrSellButton && isMySell && (
-          <StyledButton
-            variant="contained"
-            style={{ margin: '8px 0' }}
-            onClick={() => {
-              onCancelMarketItem(item.id)
-            }}
-          >
-            {t('Cancel')}
-          </StyledButton>
-        )}
-        {account && showBuyOrSellButton && !isMySell && (
-          <StyledButton
-            variant="contained"
-            style={{ margin: '8px 0' }}
-            onClick={() => {
-              onClose && onClose()
-              if (isSell && isApprove) {
-                if (!sellPrice) {
-                  alertMessage(t('Error'), t('Please fill input'), 'error')
-                }
-                if (sellPrice && parseFloat(sellPrice) == 0) {
-                  alertMessage(t('Error'), t('Sell price must greater than 0'), 'error')
-                }
-                onSell(item, sellPrice)
-              } else {
-                onBuy(item)
-              }
-            }}
-          >
-            {isSell && isApprove ? t('Sell') : t('Buy')}
-          </StyledButton>
-        )}
-      </Box>
-      {history && (
-        <Box
-          style={{
-            background: '#334756',
-            width: '100%',
-            borderRadius: '0 0 12px 12px',
-            minHeight: '100px',
-            padding: '16px',
-            overflow: 'auto',
-          }}
-        >
+      <StyledCard {...props}>
+        <Box padding="16px" width="100%">
           <Box display="flex" justifyContent="space-between">
-            <Typography fontSize="18px" color="#ffffff" fontWeight={500}>
-              {t('Buyer')}
-            </Typography>
-            <Typography fontSize="18px" color="#ffffff" fontWeight={500}>
-              {t('Price & Time')}
-            </Typography>
-          </Box>
-          <Box marginTop="8px" flex={1}>
-            {histories.length ? (
-              histories.map((item, index) => {
-                return (
-                  <Box display="flex" justifyContent="space-between">
-                    <Typography fontSize="14px" color="#ffffff" fontWeight={500}>
-                      {`${item.buyer.slice(0, 6)}...${item.buyer.slice(item.buyer.length - 4, item.buyer.length)}`}
-                    </Typography>
-                    <Typography fontSize="14px" color="#ffffff" fontWeight={500}>
-                      {item.price} ETH ({item.time})
-                    </Typography>
-                  </Box>
-                )
-              })
-            ) : (
-              <Box display="flex" justifyContent="space-between" textAlign="center">
-                <Typography fontSize="14px" color="#ffffff" fontWeight={500}>
-                  {t('No history')}
+            <Box>
+              <div># {item.tokenId}</div>
+              <Box display="flex" alignItems="flex-end" marginTop="4px">
+                {icon}
+                <Typography marginLeft="4px" fontSize="14px" lineHeight="normal">
+                  {Object.keys(ClassItem).map((i) => {
+                    if (ClassItem[i] === item.class) {
+                      return t(i)
+                    }
+                    return ''
+                  })}
                 </Typography>
+              </Box>
+            </Box>
+            {isMyNft && showBuyOrSellButton && (
+              <Box>
+                <MI.ControlPoint
+                  cursor="pointer"
+                  fontSize="large"
+                  onClick={() => {
+                    onLevelUp(item.tokenId)
+                  }}
+                />
               </Box>
             )}
+            <Box>
+              <Box display="flex">
+                <Box display="flex" alignItems="flex-start">
+                  <MI.Favorite fontSize="small" style={{ fill: '#3ac279' }} />
+                  <Typography fontSize="16px" lineHeight="normal">
+                    {item.heath}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="flex-start">
+                  <MI.FlashOn fontSize="small" style={{ fill: '#f7ac0a' }} />
+                  <Typography fontSize="16px" lineHeight="normal">
+                    {item.speed}
+                  </Typography>
+                </Box>
+              </Box>
+              <Box display="flex">
+                <Box display="flex" alignItems="flex-start">
+                  <MI.StarRate fontSize="small" style={{ fill: '#9166e0' }} />
+                  <Typography fontSize="16px" lineHeight="normal">
+                    {item.skill}
+                  </Typography>
+                </Box>
+                <Box display="flex" alignItems="flex-start">
+                  <Typography fontSize="16px" lineHeight="normal">
+                    {item.morale}
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
           </Box>
+          <Box width="100%" display="flex" flexDirection="column" justifyContent="center" alignItems="center" flex="1">
+            <StyledImage
+              style={{
+                backgroundImage: `url("${item.image}")`,
+              }}
+            ></StyledImage>
+            {/* <img src={item.image} alt="axie1" style={{ height: imageWidth ?? '160px', width: 'fit-content', margin: '8px' }} /> */}
+            {showBuyOrSellButton && isSell ? (
+              <CssTextField
+                width="50%"
+                unit="ETH"
+                type="number"
+                label={t('Price')}
+                variant="outlined"
+                myBackgroundColor="#000000"
+                myColor="#000000"
+                value={sellPrice}
+                onChange={(e) => {
+                  setSellPrice(e.target.value)
+                }}
+              />
+            ) : (
+              <Typography fontSize="20px" fontWeight={400}>
+                {item.price && item.price + ' ETH'}
+              </Typography>
+            )}
+          </Box>
+          {!account && showBuyOrSellButton && (
+            <StyledButton variant="contained" style={{ margin: '8px 0' }} onClick={connectWallet}>
+              {t('Connect Metamask')}
+            </StyledButton>
+          )}
+          {account && !isApprove && showBuyOrSellButton && isSell && (
+            <StyledButton variant="contained" style={{ margin: '8px 0' }} onClick={onApprove}>
+              {t('Approve NFT')}
+            </StyledButton>
+          )}
+          {account && showBuyOrSellButton && isMySell && (
+            <StyledButton
+              variant="contained"
+              style={{ margin: '8px 0' }}
+              onClick={() => {
+                onCancelMarketItem(item.id)
+              }}
+            >
+              {t('Cancel')}
+            </StyledButton>
+          )}
+          {account && showBuyOrSellButton && !isMySell && (
+            <StyledButton
+              variant="contained"
+              style={{ margin: '8px 0' }}
+              onClick={() => {
+                onClose && onClose()
+                if (isSell && isApprove) {
+                  if (!sellPrice) {
+                    alertMessage(t('Error'), t('Please fill input'), 'error')
+                  }
+                  if (sellPrice && parseFloat(sellPrice) == 0) {
+                    alertMessage(t('Error'), t('Sell price must greater than 0'), 'error')
+                  }
+                  onSell(item, sellPrice)
+                } else {
+                  onBuy(item)
+                }
+              }}
+            >
+              {isSell && isApprove ? t('Sell') : t('Buy')}
+            </StyledButton>
+          )}
         </Box>
-      )}
-    </StyledCard>
-  )
-})
+        {history && (
+          <Box
+            style={{
+              background: '#334756',
+              width: '100%',
+              borderRadius: '0 0 12px 12px',
+              minHeight: '100px',
+              padding: '16px',
+              overflow: 'auto',
+            }}
+          >
+            <Box display="flex" justifyContent="space-between">
+              <Typography fontSize="18px" color="#ffffff" fontWeight={500}>
+                {t('Buyer')}
+              </Typography>
+              <Typography fontSize="18px" color="#ffffff" fontWeight={500}>
+                {t('Price & Time')}
+              </Typography>
+            </Box>
+            <Box marginTop="8px" flex={1}>
+              {histories.length ? (
+                histories.map((item, index) => {
+                  return (
+                    <Box display="flex" justifyContent="space-between">
+                      <MI.CopyAllSharp onClick={() => copyBuyer(item.buyer)} fontSize="big" style={ { fill: '#c23a3a' , cursor: "pointer" } } />
+                      <Typography fontSize="14px" color="#ffffff" fontWeight={500}>
+                        {`${item.buyer.slice(0, 6)}...${item.buyer.slice(item.buyer.length - 4, item.buyer.length)}`}
+                      </Typography>
+                      <Typography fontSize="14px" color="#ffffff" fontWeight={500}>
+                        {item.price} ETH ({item.time})
+                      </Typography>
+                      <MI.LinkSharp  onClick={() => inforTx(item.buyer)} fontSize="big" style={{ fill: '#c23a3a', cursor: "pointer"}} />
+                     </Box>
+                  )
+                })
+              ) : (
+                <Box display="flex" justifyContent="space-between" textAlign="center">
+                  <Typography fontSize="14px" color="#ffffff" fontWeight={500}>
+                    {t('No history')}
+                  </Typography>
+                </Box>
+              )}
+            </Box>
+          </Box>
+        )}
+      </StyledCard>
+    )
+  })
